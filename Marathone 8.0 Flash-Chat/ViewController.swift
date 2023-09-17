62a37df5a9fc7eec8ff1e72a89f0a97c781d0a4e
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-17 12:48:38 +0500

    Initial Commit

---
+++ Marathone 8.0 Flash-Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathone 8.0 Flash-Chat
+//
+//  Created by Ислам Пулатов on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
